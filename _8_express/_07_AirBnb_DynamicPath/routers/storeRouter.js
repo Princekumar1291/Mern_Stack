b93a8914ea5a915c14dc0e26651029807e82a1c7
Prince <princekumar7320918928@gmail.com>
2024-11-05 10:41:14 +0530

    upd

--- _8_express/_07_AirBnb_DynamicPath/routers/storeRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getHome } = require('../controllers/storeController');
-const storeRouter = express.Router();
-
-storeRouter.get('/',getHome);
-
-module.exports = storeRouter;
