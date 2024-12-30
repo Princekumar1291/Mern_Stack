66f9a9119b865deacbd8e79dba79929397af6cef
Prince <princekumar7320918928@gmail.com>
2024-12-30 17:40:17 +0530

    upd

--- _06_react/_06_fetchData/src/App.jsx
+++ _06_react/_06_fetchData/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', { signal })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+
+    // Cleanup function
+    return () => {
+      controller.abort();
+    };
+  }, []); // Add empty dependency array to prevent infinite re-fetching
+
   return (
     <div>
-      app
+      {data.length == 0 ? "loading..." : <p>Data is not empty!</p>}
+      {data.map((item) => {
+        return <p key={item.id}>{item.title}</p>
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
