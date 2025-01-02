360b3c56a01ea102fecaf7cd250b66f1b30b326f
Prince <princekumar7320918928@gmail.com>
2025-01-02 15:36:53 +0530

    require.main.filename

--- _8_express/_03_AirBnb/utils.js/path-util.js
+++ _8_express/_03_AirBnb/utils.js/path-util.js
@@ -1,3 +1,10 @@
 const path = require('path');
 const rootDir = path.dirname(require.main.filename);
 module.exports = rootDir;
+
+
+// Feature                                         	require.main.filename	                                              process.cwd()
+// Points To	                                       The directory of the entry module	                                  The current working directory
+// Behavior in Testing	                             Can be inconsistent	                                                Consistent (if tests are run from the root)
+// Dynamic Entry Points	                             Sensitive to changes	                                                Independent
+// Subdirectory Execution	                           Works consistently	                                                  Depends on where node is running
